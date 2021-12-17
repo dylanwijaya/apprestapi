55c00f906320e08fb1938b85df4bdc10820d9560
unknown <locadmin@A1-PF312K5G.biznetgio.com>
2021-12-17 18:06:53 +0700

    benerin controller.js

--- controller.js
+++ controller.js
@@ -4,5 +4,5 @@ var response = require('./res')
 var connecttion = require('./koneksi')
 
 exports.index = function(req,res){
-    response.ok("Aplikasi REST API ku berjalan")
+    response.ok("Aplikasi REST API ku berjalan", res)
 }
